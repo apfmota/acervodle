0078f5034e5bf79063c49926c177438f3f72bb5b
Andriel Prieto <apmota@inf.ufsm.br>
2025-09-30 21:59:18 -0300

    fix: filter out murals and sculptures with no title

--- src/taincan/taincanAPI.js
+++ src/taincan/taincanAPI.js
@@ -4,6 +4,8 @@ const COLLECTION_URL = API_URL + "/collection/2174"
 export const MURALS_METAQUERY = "metaquery[0][key]=2200&metaquery[0][compare]=LIKE&metaquery[0][value]=.MUR.";
 export const SCULPTURES_METAQUERY = "metaquery[0][key]=2200&metaquery[0][compare]=LIKE&metaquery[0][value]=.ESC.";
 
+export const NO_TITLE_FILTER_METAQUERY = "metaquery[1][key]=2177&metaquery[1][compare]=!=&metaquery[1][value]=Sem Título"
+
 var cachedMetadata = []
 export const getCollectionMetadata = async () => {
     if (cachedMetadata.length == 0) {
@@ -38,11 +40,11 @@ export const countElements = async (metaqueryParams = "") => {
 }
 
 export const countMurals = async () => {
-    return await countElements(MURALS_METAQUERY);
+    return await countElements(MURALS_METAQUERY + "&" + NO_TITLE_FILTER_METAQUERY);
 }
 
 export const countSculptures = async () => {
-    return await countElements(SCULPTURES_METAQUERY);
+    return await countElements(SCULPTURES_METAQUERY + "&" + NO_TITLE_FILTER_METAQUERY);
 }
 
 const elementsCache = {};
@@ -70,11 +72,11 @@ export const getAllArts = async () => {
 }
 
 export const getAllMurals = async () => {
-    return await getElements(MURALS_METAQUERY);
+    return await getElements(MURALS_METAQUERY + "&" + NO_TITLE_FILTER_METAQUERY);
 }
 
 export const getAllSculptures = async () => {
-    return await getElements(SCULPTURES_METAQUERY);
+    return await getElements(SCULPTURES_METAQUERY + "&" + NO_TITLE_FILTER_METAQUERY);
 }
 
 const artDataCache = {};
