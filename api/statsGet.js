466ff71381ede0186e32fb88b2eaeb55a3a4583b
Felipe <felipebecker840@gmail.com>
2025-10-30 13:04:13 -0300

    Adiciona API e banco de dados

---
+++ api/statsGet.js
@@ -0,0 +1,29 @@
+import clientPromise from '../lib/mongodb.js';
+
+export default async function handler(req, res) {
+
+  if (req.method !== 'GET') {
+    res.setHeader('Allow', ['GET']);
+    return res.status(405).end(`Método ${req.method} Não Permitido`);
+  }
+
+  const { date } = req.query;
+
+  if (!date) {
+    return res.status(400).json({ error: 'Parâmetro "date" é obrigatório' });
+  }
+
+  try {
+    const client = await clientPromise;
+    const db = client.db(process.env.MONGODB_DATABASE); 
+    const collection = db.collection(process.env.MONGODB_COLLECTION);
+
+    const statsDocument = await collection.findOne({ _id: date });
+
+    res.status(200).json(statsDocument);
+
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: 'Erro ao conectar ou buscar no banco de dados' });
+  }
+}
