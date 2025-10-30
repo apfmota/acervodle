813523e0851b8e883e2441889e728ff128574351
Felipe <felipebecker840@gmail.com>
2025-10-30 13:09:52 -0300

    Remove hardcoded

--- api/statsPost.js
+++ api/statsPost.js
@@ -25,10 +25,10 @@ export default async function handler(req, res) {
     }
 
     try {
-        
+
         const client = await clientPromise;
-        const db = client.db(process.env.MONGODB_DBNAME || "Acervodle");
-        const collection = db.collection("Acervodle_daily_statistics");
+        const db = client.db(process.env.MONGODB_DATABASE);
+        const collection = db.collection(process.env.MONGODB_COLLECTION);
 
         const filter = { "_id": date };
         
